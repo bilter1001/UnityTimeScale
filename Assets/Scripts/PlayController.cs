1b02fffd63caa3ad9a010c251ce0d1d781e16b1b
shenzhixiong_313322 <bilter1001@163.com>
2020-06-15 15:53:30 +0800

    添加timescale的测试工程和代码

---
+++ Assets/Scripts/PlayController.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+using UnityEngine.Timeline;
+
+namespace PlayControllerScript
+{
+    public class PlayController
+    {
+        private static PlayController _instance;
+
+        public static PlayController Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new PlayController();
+                }
+
+                return _instance;
+            }
+        }
+
+
+        Dictionary<VideoPlayer, float> dicVideoPlayer = new Dictionary<VideoPlayer, float>();
+        Dictionary<AudioSource, float> dicAudioSource = new Dictionary<AudioSource, float>();
+                
+
+        public void SetVideoAudioSpeed(float speed)
+        {
+            dicVideoPlayer.Clear();
+            dicAudioSource.Clear();
+            VideoPlayer[] arrVideoPlayer = GameObject.FindObjectsOfType<VideoPlayer>();
+            foreach (var itemVideo in arrVideoPlayer)
+            {
+                dicVideoPlayer[itemVideo] = itemVideo.playbackSpeed;
+                itemVideo.playbackSpeed *= speed;
+            }
+
+            AudioSource[] arrAudioSource = GameObject.FindObjectsOfType<AudioSource>();
+            foreach (var itemAudio in arrAudioSource)
+            {
+                dicAudioSource[itemAudio] = itemAudio.pitch;
+                itemAudio.pitch *= speed;
+            }
+        }
+
+        public void ReSetVideoAudioSpeed()
+        {
+            VideoPlayer[] arrVideoPlayer = GameObject.FindObjectsOfType<VideoPlayer>();
+            foreach (var itemVideo in arrVideoPlayer)
+            {
+
+                float oriSpeed = 1;
+                if (dicVideoPlayer.TryGetValue(itemVideo, out oriSpeed))
+                {
+                    itemVideo.playbackSpeed = oriSpeed;
+                }
+                else
+                {
+                    itemVideo.playbackSpeed = 1;
+                }
+
+            }
+
+            AudioSource[] arrAudioSource = GameObject.FindObjectsOfType<AudioSource>();
+            foreach (var itemAudio in arrAudioSource)
+            {
+                float oriPitch = 1;
+                if (dicAudioSource.TryGetValue(itemAudio, out oriPitch))
+                {
+                    itemAudio.pitch = oriPitch;
+                }
+                else
+                {
+                    itemAudio.pitch = 1;
+                }
+            }
+        }
+
+        public void ChangeTimelineSpeed(float speed)
+        {
+            //获取所有场景中的PlayableDirector
+            List<PlayableDirector> directors = new List<PlayableDirector>();
+            //检索所有的PlayableDirector
+            for (int i = 0; i < SceneManager.sceneCount; i++)
+            {
+                var scene = SceneManager.GetSceneAt(i);
+                var roots = scene.GetRootGameObjects();
+                if (roots == null)
+                {
+                    continue;
+                }
+                foreach (var root in roots)
+                {
+                    var components = root.GetComponentsInChildren<PlayableDirector>();
+                    if (components != null)
+                    {
+                        directors.AddRange(components);
+                    }
+                }
+            }
+            SetDirectorsSpeed(directors, speed);
+        }
+
+        void SetDirectorsSpeed(List<PlayableDirector> directors, float speed)
+        {
+            //检索所有的轨道
+            if (directors == null || directors.Count <= 0)
+            {
+                return;
+            }
+
+            foreach (var director in directors)
+            {
+                if (director == null || director.playableAsset == null)
+                {
+                    continue;
+                }
+
+                var playableBindings = director.playableAsset.outputs.ToList();
+                if (!playableBindings.Any())
+                {
+                    continue;
+                }
+
+                ////遍历track，找到音频轨道
+                //foreach (var playableBinding in playableBindings)
+                //{
+                //    if (playableBinding.sourceObject is AudioTrack)
+                //    {
+                //        AudioTrack audioTrack = playableBinding.sourceObject as AudioTrack;
+                //        //遍历clip
+                //        foreach (var clip in audioTrack.GetClips())
+                //        {
+                //            if (clip.asset is AudioPlayableAsset)
+                //            {
+                //                clip.timeScale = speed; //设置该TimelineClip的SpeedMultiplier
+                //                Debug.LogError("clip.timeScale: " + clip.timeScale);
+                //            }
+                //        }
+                //    }
+                //}
+
+                PlayableGraph? playableGraph = director.playableGraph;
+                if (playableGraph != null && ((PlayableGraph)playableGraph).IsValid())
+                {
+                    int rootCount = director.playableGraph.GetRootPlayableCount();
+                    for (int i = 0; i < rootCount; i++)
+                    {
+                        director.playableGraph.GetRootPlayable(i).SetSpeed(speed);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TimeScaleTest.cs
@@ -0,0 +1,93 @@
+﻿using PlayControllerScript;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeScaleTest : MonoBehaviour
+{
+    public float TimescalParam = 1;
+    private float _lastUpdateTime = 0;
+    private float _lastLateUpdateTime = 0;
+    private float _lastFixedUpdateTime = 0;
+
+    private float _startRealtime = 0;
+    private float _startTime = 0;
+    private bool IsFirstCountRealtime = true;
+
+    public InputField IFTimeScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startRealtime = Time.realtimeSinceStartup;
+        _startTime = Time.time;
+        IsFirstCountRealtime = true;
+        Time.timeScale = TimescalParam;
+        Debug.LogError("_startRealtime : " + _startRealtime);
+        Debug.LogError("_startTime : " + _startTime);
+        Debug.LogError("Time.timeScale : " + Time.timeScale);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsFirstCountRealtime  && (Time.time - _startTime) > 5)
+        {
+            float timeInterval = Time.time - _startTime;
+
+            Debug.LogError("RealTime interval : " + (Time.realtimeSinceStartup - _startRealtime));
+            Debug.LogError("Time interval : " + timeInterval);
+            IsFirstCountRealtime = false;
+        }
+        float realTimeInterval = Time.realtimeSinceStartup - _lastUpdateTime;
+        Debug.Log("----------------------------");
+        Debug.Log("[Update time interval:]" + realTimeInterval);
+        Debug.Log("Update Time.deltaTime: " + Time.deltaTime);
+        Debug.Log("Update Time.time: " + Time.time);
+        _lastUpdateTime = Time.realtimeSinceStartup;
+    }
+
+    void LateUpdate()
+    {
+        float realTimeInterval = Time.realtimeSinceStartup - _lastLateUpdateTime;
+        Debug.Log("----------------------------");
+        Debug.Log("[LateUpdate time interval:]" + realTimeInterval);
+        Debug.Log("LateUpdate Time.deltaTime: " + Time.deltaTime);
+        Debug.Log("LateUpdate Time.time: " + Time.time);
+        _lastLateUpdateTime = Time.realtimeSinceStartup;
+    }
+
+    void FixedUpdate()
+    {
+        float realTimeInterval = Time.realtimeSinceStartup - _lastFixedUpdateTime;
+        Debug.Log("----------------------------");
+        Debug.Log("[FixedUpdate time interval:]" + realTimeInterval);
+        Debug.Log("FixedUpdate Time.fixedDeltaTime : " + Time.fixedDeltaTime);
+        Debug.Log("FixedUpdate Time.time: ]" + Time.time);
+        _lastFixedUpdateTime = Time.realtimeSinceStartup;
+    }
+
+    public void SetTimeScale()
+    {
+        if (IFTimeScale != null)
+        {
+            if (!string.IsNullOrEmpty(IFTimeScale.text))
+            {
+                TimescalParam = float.Parse(IFTimeScale.text);
+                Time.timeScale = TimescalParam;
+                Debug.LogError("Time.timeScale : " + Time.timeScale);
+            }
+        }
+
+    }
+
+    public void ChangeSpeed()
+    {
+
+        //设置音视频播放速度
+        PlayController.Instance.SetVideoAudioSpeed(Time.timeScale);
+
+        //设置时间轴速度
+        //PlayController.Instance.ChangeTimelineSpeed(Time.timeScale);
+    }
+}
